9cb3b67a7f7e14a4182d30e94c8907631ce44b8c
yulianacastro7 <yulianacastrodiaz7@gmail.com>
2021-10-14 18:42:44 -0500

    Update search for userStoryApis

--- src/preset/ticket.js
+++ src/preset/ticket.js
@@ -5,7 +5,7 @@ async function presetTickets() {
   try {
     const allTickets = await Ticket.findAll();
     const userStoryProductCategory = await User_story.findOne({ where: { title: 'producto(categoria)' } });
-    const userStoryApis = await User_story.findOne({ where: { title: "definir los 10api's candidatas" } });
+    const userStoryApis = await User_story.findOne({ where: { title: "definir las 10api's candidatas" } });
     const userStoryAccessToDb = await User_story.findOne({ where: { title: 'obtener acceso a las bases de datos' } });
     await userStoryProductCategory.addTickets([allTickets[0], allTickets[1], allTickets[2], allTickets[3], allTickets[4]]);
     await userStoryApis.addTickets([allTickets[5], allTickets[6], allTickets[7], allTickets[8]]);
