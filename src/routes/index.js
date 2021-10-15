ddd219dcc57d1ee1379854ab8d6a637fba5d2f54
yulianacastro7 <yulianacastrodiaz7@gmail.com>
2021-10-14 22:18:06 -0500

    modularized routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,4 +4,17 @@ const router = Router();
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
+const companiesRouter = require('./companies');
+const projectsRouter = require('./projects');
+const ticketsRouter = require('./tickets');
+const userStoriesRouter = require('./userStories');
+
+// Configurar los routers
+// Ejemplo: router.use('/auth', authRouter);
+
+router.use('/companies', companiesRouter);
+router.use('/project', projectsRouter);
+router.use('/tickets', ticketsRouter);
+router.use('/userstories', userStoriesRouter);
+
 module.exports = router;
