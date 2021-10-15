f4dd92934cf0178ed8997ce7d43f96b0bea531b0
yulianacastro7 <yulianacastrodiaz7@gmail.com>
2021-10-14 22:49:56 -0500

    Route POST/ticket

--- src/routes/tickets.js
+++ src/routes/tickets.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { Ticket } = require('../db');
+const { Ticket, User_story } = require('../db');
 const router = Router();
 
 try {
@@ -14,6 +14,27 @@ try {
       }
     } else {
       res.status(404).json({ msg: 'Se necesita el id de la user story' });
+    }
+  });
+} catch (error) {
+  res.status(400).json(error);
+}
+
+try {
+  router.post('/', async (req, res) => {
+    const { comment, userStoryId } = req.body;
+    if (!userStoryId) return res.status(404).json({ msg: 'Se necesita el id de la user story' });
+    if (comment) {
+      const newTicket = await Ticket.create({
+        comment
+      });
+      const userStoryDb = await User_story.findByPk(userStoryId);
+      if (!userStoryDb) return res.status(404).json({ msg: 'No existe la user story' });
+      await userStoryDb.addTicket(newTicket);
+      const aux = await Ticket.findByPk(newTicket.id);
+      res.json(aux);
+    } else {
+      res.status(404).json({ msg: 'Se necesita el comentario del ticket' });
     }
   });
 } catch (error) {
