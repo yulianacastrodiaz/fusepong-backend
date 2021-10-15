7e6f1fc31d0551bbc125316b6be1de451cce7d26
yulianacastro7 <yulianacastrodiaz7@gmail.com>
2021-10-15 16:47:48 -0500

    Update route post /signup

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -1,20 +1,21 @@
 const { Router } = require('express');
-const { User } = require('../db.js')
+const { User, Company } = require('../db');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const authConfig = require('../config/auth')
+const authConfig = require('../config/auth');
 
 const router = Router();
 try {
   router.post('/', async (req, res) => {
-    const { username, email, password, name, lastname } = req.body;
-    let passwordHashed = bcrypt.hashSync(password, parseInt(authConfig.rounds))
+    const { username, email, password, name, lastname, companyId } = req.body;
+    let passwordHashed = bcrypt.hashSync(password, parseInt(authConfig.rounds));
     if (!username) return res.status(400).json({ msg: "Ingresa un nombre de usuario" });
     if (!name) return res.status(400).json({ msg: "Ingresa un nombre" });
     if (!password) return res.status(400).json({ msg: "Ingresa una contraseña" });
     if (password.length < 6) return res.status(400).json({ msg: "La contraseña debe ser mínimo de 6 caracteres" });
     if (!lastname) return res.status(400).json({ msg: "Ingresa el apellido" });
     if (!email) return res.status(400).json({ msg: "Ingresa un mail" });
+    if (!companyId) return res.status(400).json({ msg: "Ingresa un mail" });
 
     const newUser = await User.create({
       email,
@@ -23,11 +24,18 @@ try {
       lastname,
       password: passwordHashed,
     })
+    const companyDb = await Company.findByPk(companyId);
+    if (companyDb) {
+      await companyDb.addUser(newUser);
+    } else {
+      res.status(400).json({ msg: "La compañía no existe" });
+    }
     let token = jwt.sign({ user: newUser }, authConfig.secret, {
       expiresIn: authConfig.expires
     })
+    const user = await User.findByPk(newUser.id);
     res.json({
-      user: newUser,
+      user,
       token
     })
   })
